22394e0671b234e29be731312919537776084de0
Wanying Xie <xiewanying07@gmail.com>
2019-12-24 11:17:28 +0800

    Fix the bug of

--- eval.sh
+++ eval.sh
@@ -1,4 +1,8 @@
-model=iwslt17/curr.update1.5k.finetune30
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+model=iwslt17/curr.earlystop1000
 #model=iwslt17/baseline_base
 list=`seq $1 $2`
 for i in $list
